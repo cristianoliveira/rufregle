50a9f30fafa2ad8a2bd05d60cb1a9cbc556b721d
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-02-18 11:21:22 -0200

    Removing old files

--- test/test_hola.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-require 'rufregle'
-
-class RufregleTest < Test::Unit::TestCase
-  def test_english_hello
-    assert_equal "Olá", Rufregle.translate("Hello","en","pt")[0][0]
-  end
-end
