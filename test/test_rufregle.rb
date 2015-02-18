0ed36ed6f4c0aca66c80589bc250a9b3a3aaa7f2
Cristian Oliveira <contato@cristianoliveira.com.br>
2015-02-18 11:24:05 -0200

    Fix tests

--- test/test_rufregle.rb
+++ test/test_rufregle.rb
@@ -4,28 +4,28 @@ require './lib/rufregle.rb'
 class RufregleTest < Test::Unit::TestCase
 
   def test_english_to_portuguese_hello
-    assert_equal "Olá", Rufregle.new.translate("Hello", :en , :pt)
+    assert_equal("Olá", Rufregle.new.translate("Hello", :en , :pt))
   end
 
   def test_prhase_translate
-    assert_equal "This is a translated phrase", Rufregle.new.translate('Isso é uma frase traduzida', :pt, :en)
+    assert_equal("This is a translated phrase", Rufregle.new.translate('Isso é uma frase traduzida', :pt, :en))
   end
 
   def test_cached_translate
     r = Rufregle.new
     translate = r.translate("That is cached", :en, :pt)
-    assert_equal translate, r.get_cached_translate.last
+    assert_equal(translate, r.get_cached_translate.last)
   end
 
   def test_clean_cache
     r = Rufregle.new
     translate = r.translate("That is cached", :en, :pt)
     r.clean_cache
-    assert_true r.get_cached_translate.empty?
+    assert_true(r.get_cached_translate.empty?)
   end
 
   def test_static_call
-    assert_equal "static call", Rufregle::translate('chamada estática', :pt, :en)
+    assert_equal("static call", Rufregle::translate('chamada estática', :pt, :en))
   end
 
   def test_invalid_params
